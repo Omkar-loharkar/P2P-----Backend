6e79b08de8e53c0fe0a28bbb583682969bd9b329
fahad <fahadmd55555@gmail.com>
2025-08-12 20:52:29 +0530

    added api in security config get post working fine

--- src/main/java/com/p2p/retailstore/web/RetailStoreController.java
+++ src/main/java/com/p2p/retailstore/web/RetailStoreController.java
@@ -5,7 +5,10 @@ import com.p2p.ecomm.service.CartService;
 import com.p2p.retailstore.model.domain.Request.RetailProductRequestDTO;
 import com.p2p.retailstore.model.domain.Response.RetailProductResponseDTO;
 import com.p2p.retailstore.service.RetailProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -32,8 +35,10 @@ public class RetailStoreController {
         return service.getProductById(id);
     }
 
+    @Operation(summary = "Create/Add Product For Retail Store",
+    description = "This API adds details of product master")
     @PostMapping
-    public RetailProductResponseDTO create(@RequestHeader("token") String token, @RequestBody RetailProductRequestDTO dto) {
+    public RetailProductResponseDTO create( @RequestHeader("token") String token, @RequestBody RetailProductRequestDTO dto) {
         return service.createRetailProduct(dto);
     }
 
