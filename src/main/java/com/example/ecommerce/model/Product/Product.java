796dc2303908e4c34a5a04561284cdba59378f3e
Omkar Rajendra Loharkar <omkarloharkar21@gmail.com>
2025-08-12 14:10:05 +0530

    Added Lombok To Project

--- src/main/java/com/example/ecommerce/model/Product/Product.java
+++ src/main/java/com/example/ecommerce/model/Product/Product.java
@@ -1,9 +1,13 @@
 package com.example.ecommerce.model.Product;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,16 +16,4 @@ public class Product {
     private double price;
     private String image;
     private String description;
-
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-    public String getImage() { return image; }
-    public void setImage(String image) { this.image = image; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
 } 
--- src/main/java/com/example/ecommerce/model/User/User.java
+++ src/main/java/com/example/ecommerce/model/User/User.java
@@ -1,9 +1,13 @@
 package com.example.ecommerce.model.User;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,14 +19,4 @@ public class User {
     private String email;
 
     private String password;
-
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 } 
