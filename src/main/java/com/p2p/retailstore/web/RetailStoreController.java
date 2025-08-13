fe32d00eee821720a2a2926bb080566d80ab8cd0
Omkar-loharkar <168171131+Omkar-loharkar@users.noreply.github.com>
2025-08-13 14:45:42 +0530

    Removed Unwanted Imports

--- src/main/java/com/p2p/retailstore/web/RetailStoreController.java
+++ src/main/java/com/p2p/retailstore/web/RetailStoreController.java
@@ -1,18 +1,12 @@
 package com.p2p.retailstore.web;
 
-import com.p2p.ecomm.model.CartItem;
-import com.p2p.ecomm.service.CartService;
 import com.p2p.retailstore.model.domain.Request.RetailProductRequestDTO;
 import com.p2p.retailstore.model.domain.Response.RetailProductResponseDTO;
 import com.p2p.retailstore.service.RetailProductService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Description;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/retailstore/products")
